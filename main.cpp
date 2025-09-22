ef28a082c269e46f3816e018bb5c411c5b38cfd1
EduardoCoOliveira <eduardocordeirooliveira@alunos.utfpr.edu.br>
2025-09-22 18:55:36 -0300

    classe main adicionada

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by eduol on 22/09/2025.
+//
+
+#include "main.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by eduol on 22/09/2025.
+//
+
+#ifndef ESCALONADOR_DE_SO_MAIN_H
+#define ESCALONADOR_DE_SO_MAIN_H
+
+
+class main
+{
+};
+
+
+#endif //ESCALONADOR_DE_SO_MAIN_H
